76c0bd811eefb24c5454a9903ca37cbf5aef56b6
David Days <david.c.days@gmail.com>
2017-11-11 16:03:56 -0500

    refactoring, setting up CLion

---
+++ code/graphdata/include/graphinit.h
@@ -0,0 +1,11 @@
+//
+// Created by david on 11/11/17.
+//
+
+#ifndef GRAPHDATA_GRAPHINIT_H
+#define GRAPHDATA_GRAPHINIT_H
+
+
+
+
+#endif //GRAPHDATA_GRAPHINIT_H
---
+++ code/graphdata/src/graphinit.c
@@ -0,0 +1,4 @@
+//
+// Created by david on 11/11/17.
+//
+
