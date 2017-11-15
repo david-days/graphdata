72ce6076e011652d53c0f3fc75b60429a7f26797
David C. Days <david.c.days@gmail.com>
2017-11-14 22:40:45 -0500

    Added CUnit tests; successful initial test run

--- code/graphdata/test/src/comptests.c
+++ code/graphdata/test/src/comptests.c
@@ -5,8 +5,48 @@
 #include <CUnit/Basic.h>
 #include <util/graphcomp.h>
 
-void simpleTest(void) {
-    CU_ASSERT(1 == 1);
+void minNodeTest(void) {
+    size_t big = 1000;
+    size_t small = 5;
+    size_t *result = NULL;
+    result = minNode(&big, &small);
+    CU_ASSERT(result == &small);
+    result = NULL;
+    result = minNode(&small, &big);
+    CU_ASSERT(result == &small);
+}
+
+void maxNodeTest(void) {
+    size_t big = 1000;
+    size_t small = 5;
+    size_t *result = NULL;
+    result = maxNode(&big, &small);
+    CU_ASSERT(result == &big);
+    result = NULL;
+    result = maxNode(&small, &big);
+    CU_ASSERT(result == &big);
+}
+
+void minValTest(void) {
+    double big = 3200.0;
+    double small = 12.0;
+    double *result = NULL;
+    result = minVal(&big, &small);
+    CU_ASSERT(result == &small);
+    result = NULL;
+    result = minVal(&small, &big);
+    CU_ASSERT(result == &small);
+}
+
+void maxValTest(void) {
+    double big = 3200.0;
+    double small = 12.0;
+    double *result = NULL;
+    result = maxVal(&big, &small);
+    CU_ASSERT(result == &big);
+    result = NULL;
+    result = maxVal(&small, &big);
+    CU_ASSERT(result == &big);
 }
 
 int main (int argc, char** argv) {
@@ -14,13 +54,28 @@ int main (int argc, char** argv) {
     if (CUE_SUCCESS != CU_initialize_registry())
         return CU_get_error();
 
-    pSuite = CU_add_suite("Test Suite 1", NULL, NULL);
+    pSuite = CU_add_suite("MinMax Suite", NULL, NULL);
     if (NULL == pSuite) {
         CU_cleanup_registry();
         return CU_get_error();
     }
 
-    if (NULL == CU_add_test(pSuite, "Simple Test Example", simpleTest)) {
+    if (NULL == CU_add_test(pSuite, "Min size_t", minNodeTest)) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    if (NULL == CU_add_test(pSuite, "Max size_t", maxNodeTest)) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    if (NULL == CU_add_test(pSuite, "Min double", minValTest)) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    if (NULL == CU_add_test(pSuite, "Max double", maxValTest)) {
         CU_cleanup_registry();
         return CU_get_error();
     }
