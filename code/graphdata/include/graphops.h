09c0ad78f66e8b1d5c811af49f73e0d8a7d93a1b
David C. Days <david.c.days@gmail.com>
2017-11-16 11:30:39 -0500

    Fixing documentation

--- code/graphdata/include/graphops.h
+++ code/graphdata/include/graphops.h
@@ -9,19 +9,21 @@
 
 //Read functions to extract data
 /**
- * Function pointer definition for getting the node count;
+ * @brief Function pointer definition for getting the node count;
  * @param g Graph structure in question
  * @return Count of nodes, if graph is not null; otherwise, return 0
  */
 typedef size_t (*funcNodeCount)(graph *g);
 /**
- * Function pointer to extract count of edges
+ * @brief Function pointer to extract count of edges
  * @param g Graph structure in question
  * @return Count of edges, if graph is not null; otherwise, return 0
  */
 typedef size_t (*funcEdgeCount)(graph *g);
 /**
- * Function pointer to retrieve a node structure reference.  Implementation-specific on whether this
+ * @brief Function pointer to retrieve a node structure reference.
+ *
+ * Implementation-specific on whether this
  * structure is part of the original graph (LINK-based implementations) or must use free() when usage is finished (ARRAY-based).
  * @param nodeid Identifier of the node to be retrieved
  * @param g Graph structure in question
@@ -29,7 +31,9 @@ typedef size_t (*funcEdgeCount)(graph *g);
  */
 typedef node * (*funcGetNode)(const size_t *nodeid, const graph *g);
 /**
- * Function pointer to retrieve a edge structure reference.  Implementation-specific on wheter this structure is part of the
+ * @brief Function pointer to retrieve a edge structure reference.
+ *
+ * Implementation-specific on whether this structure is part of the
  * original graph (LINK-based implementations) or must use free() when usage is finished (ARRAY-based).
  * @param u nodeid of the starting edge.
  * @param v nodeid of the ending edge.
@@ -38,24 +42,25 @@ typedef node * (*funcGetNode)(const size_t *nodeid, const graph *g);
  */
 typedef edge * (*funcGetEdge)(const size_t *u, const size_t *v, const graph *g);
 /**
- * Function pointer to retrieve linked-list of nodes that are currently defined as neighbors to the given node.  Returned linked-list is distinct from
- * the graph structure, and consumers must use free() when finished.
+ * @brief Function pointer to retrieve linked-list of nodes that are currently defined as neighbors to the given node.
+ *
+ * Returned linked-list is distinct from the graph structure, and consumers must use free() when finished.
  * @param nodeid Identifier of the node in question
  * @param g Graph structure in question
  * @return linked-list of node references, starting with the given node, if found; otherwise, pointer to NULL.
  */
 typedef node * (*funcGetNeighbors)(const size_t *nodeid, const graph *g);
 /**
- * Function pointer to retrieve linked-list of edges from a given node.  Returned linked-list is distinct from
- * the graph structure, and consumers must use free() when finished.
+ * @brief Function pointer to retrieve linked-list of edges from a given node.
+ * Returned linked-list is distinct from the graph structure, and consumers must use free() when finished.
  * @param nodeid Identifier of the node in question
  * @param g Graph structure in question
  * @return linked-list of edges starting from the given node, if found; otherwise, pointer to NULL.
  */
 typedef edge * (*funcGetEdges)(const size_t *nodeid, const graph *g);
 /**
- * Function pointer to retrieve linked-list path of nodes from start to destination, beginning with the starting node.  Returned linked-list
- * is distinct from the graph structure, and consumers must use free() when finished.
+ * @brief Function pointer to retrieve linked-list path of nodes from start to destination, beginning with the starting node.
+ * Returned linked-list is distinct from the graph structure, and consumers must use free() when finished.
  * @param uid Path start identifier
  * @param vid Path end identifier
  * @param g Graph in question
@@ -63,8 +68,8 @@ typedef edge * (*funcGetEdges)(const size_t *nodeid, const graph *g);
  */
 typedef node * (funcGetNodePath)(const size_t *uid, const size_t *vid, const graph *g);
 /**
- * Function pointer to retrieve linked-list of path edges from start to destination, beginning with the starting node.  Returned linked-list is
- * distinct from the graph structure, and consumers must use free() when finished.
+ * @brief Function pointer to retrieve linked-list of path edges from start to destination, beginning with the starting node.
+ * Returned linked-list is distinct from the graph structure, and consumers must use free() when finished.
  * @param uid Path start identifier
  * @param vid Path end identifier
  * @param g Graph in question
@@ -74,16 +79,16 @@ typedef edge * (funcGetEdgePath)(const size_t *uid, const size_t *vid, const gra
 
 //Write functions to modify graph
 /**
- * Function pointer to add a node to a given graph.  Not all implementations may use this (for example, fixed-size graphs such as ARRAY
- * implementations).
+ * @brief Function pointer to add a node to a given graph.
+ * Not all implementations may use this (for example, fixed-size graphs such as spatial ARRAY implementations).
  * @param nodeid Node identifier to be added
  * @param g Graph structure to add the node
  * @return -1 if there was an error, 0 if the node was successfully added, 1 if the node already exists.
  */
 typedef int (*funcAddNode)(const size_t *nodeid, graph *g);
 /**
- * Function pointer to add an edge to a given graph.  Not all implementations may use this (for example, fixed-size ARRAY implementations
- * representing a set domain of nodes and relationships).
+ * @brief Function pointer to add an edge to a given graph.
+ * Not all implementations may use this (for example, fixed-size ARRAY implementations representing a set domain of nodes and relationships).
  * @param uid identifer for start of edge
  * @param vid identifier for end of edge
  * @param cap capacity value to be assigned
@@ -92,7 +97,7 @@ typedef int (*funcAddNode)(const size_t *nodeid, graph *g);
  */
 typedef int (*funcAddEdge)(const size_t *uid, const size_t *vid, double *cap, graph *g);
 /**
- * Function pointer to set the capacity (cost, weight, etc.) of an edge in the given graph.
+ * @brief Function pointer to set the capacity (cost, weight, etc.) of an edge in the given graph.
  * @param uid identifier of the edge start
  * @param vid identifier of the edge ending.
  * @param cap capacity value to be set
@@ -103,6 +108,8 @@ typedef int (*funcSetCapacity)(const size_t *uid, const size_t *vid, const doubl
 
 
 /**
+ * @brief Structure relating graph data to implementations
+ *
  * This is the structure containing both a reference to the graph data and the supporting basic operations.
  * Function pointers (according to the typedefs) are set if there is a function implementation associated with the type
  * of graph and implementation.
@@ -110,43 +117,45 @@ typedef int (*funcSetCapacity)(const size_t *uid, const size_t *vid, const doubl
 struct graphops_t {
     //Graph structure
     /**
-     * Graph data structure
+     * @brief Graph data structure
      */
     graph *g;
 
     //Getters
     /**
-     * Return count of nodes
+     * @brief Return count of nodes
      */
     funcNodeCount nodeCount;
     /**
-     * Return count of edges
+     * @brief Return count of edges
      */
     funcEdgeCount edgeCount;
     /**
-     * Return a given node (by identifier)
+     * @brief Return a given node (by identifier)
      */
     funcGetNode getNode;
     /**
-     * Return a given edge (by start-end identifiers)
+     * @brief Return a given edge (by start-end identifiers)
      */
     funcGetEdge getEdge;
     /**
-     * Return a linked-list of neighbors
+     * @brief Return a linked-list of neighbors
      */
     funcGetNeighbors getNeighbors;
     /**
-     * Return a linked-list of edges starting at this node
+     * @brief Return a linked-list of edges starting at this node
      */
     funcGetEdges getEdges;
 
     //Modifiers
     /**
-     * Add a node to the graph.  Expected to be NULL (no implementation) for fixed-size graph structures.
+     * @brief Add a node to the graph.
+     * Expected to be NULL (no implementation) for fixed-size graph structures.
      */
     funcAddNode addNode;
     /**
-     * Add an edge to the graph.  Expected to be NULL (no implementation) for fixed-size graph structures.
+     * @brief Add an edge to the graph.
+     * Expected to be NULL (no implementation) for fixed-size graph structures.
      */
     funcAddEdge addEdge;
     /**
@@ -156,11 +165,13 @@ struct graphops_t {
 
     //Traverse functions
     /**
-     * Return a list of node (hops) from the start to the end.  Means of finding the path is implementation-specific
+     * @brief Return a list of node (hops) from the start to the end.
+     * Means of finding the path is implementation-specific
      */
     funcGetNodePath nodePath;
     /**
-     * Return a list of edges traversing from the start to finish.  Means of finding the path is implementation-specific.
+     * @brief Return a list of edges traversing from the start to finish.
+     * Means of finding the path is implementation-specific.
      */
     funcGetEdgePath edgePath;
 };
